800a66bd9ac096816531f52220a68581dce91977
Maev4l <maeval.nightingale@gmail.com>
2019-06-01 09:28:50 +0200

    Refactor to 'hexagonal' architecture

--- contacts/handler.py
+++ contacts/handler.py
@@ -1,116 +1,53 @@
 import json
 import logging
-import os
-from contextlib import closing
+
+
 import uuid
-import psycopg2
-from psycopg2.extras import NamedTupleCursor
+
+
 from contacts.utils import makeResponse
 from contacts.utils import get_attribute
+from contacts.storage import get_storage
 
 logger = logging.getLogger()
 
 
-def getConnection():
-    hostname = os.environ['PGHOST']
-    port = os.environ['PGPORT']
-    database = os.environ['PGDATABASE']
-    user = os.environ['PGUSER']
-    password = os.environ['PGPASSWORD']
-
-    connection = psycopg2.connect(
-        host=f'{hostname}',
-        port=f'{port}',
-        database=f'{database}',
-        user=f'{user}',
-        password=f'{password}'
-    )
-
-    logger.debug(f'Connected successfully to database {database}')
-    return connection
-
-
 def post_contact(event, context):
+    storage = get_storage()
     try:
-        with closing(getConnection()) as connection:
-            with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
+        user_id = event['requestContext']['authorizer']['claims']['sub']
+        payload = json.loads(event['body'])
+        id = get_attribute(payload, 'id', None)
+        if id is None:
+            id = str(uuid.uuid4())
+            storage.save_contact(user_id, id, payload)
+            return makeResponse(201, {'id': id})
+        else:
+            storage.modify_contact(user_id, payload)
+            return makeResponse(204)
 
-                user_id = event['requestContext']['authorizer']['claims']['sub']
-                payload = json.loads(event['body'])
-                schema = os.environ['PGSCHEMA']
-                nickname = get_attribute(payload, 'nickname', '')
-                thumbnail = get_attribute(payload, 'thumbnail', None)
-                id = get_attribute(payload, 'id', None)
-                if id is None:
-                    id = str(uuid.uuid4())
-                    cursor.execute(
-                        f'INSERT INTO "{schema}"."contact"(id, user_id, nickname, thumbnail) '
-                        "VALUES ( %s, %s, %s, %s); ",
-                        (id, user_id, nickname, thumbnail))
-                    connection.commit()
-                    return makeResponse(201, {'id': id})
-                else:
-                    cursor.execute(
-                        f'UPDATE "{schema}"."contact" '
-                        'SET "nickname"=%s, "thumbnail"=%s WHERE "id" = %s AND "user_id"=%s ;',
-                        (nickname, thumbnail, id, user_id))
-                    connection.commit()
-                    return makeResponse(204)
-
-    except psycopg2.Error as e:
-        logger.error(f"post contact error: {e}")
+    except Exception as e:
         return makeResponse(500, {'message': str(e)})
 
 
 def get_contacts(event, context):
+    storage = get_storage()
     try:
-        with closing(getConnection()) as connection:
-            with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
-                user_id = event['requestContext']['authorizer']['claims']['sub']
-                schema = os.environ['PGSCHEMA']
-
-                cursor.execute(
-                    f'SELECT "id", "nickname", "thumbnail" FROM "{schema}"."contact"'
-                    'WHERE "user_id"=%s ORDER BY "nickname"',
-                    [user_id])
-
-                result = {
-                    'contacts': []
-                }
-                for record in cursor:
-                    result['contacts'].append({
-                        'id': record.id,
-                        'nickname': record.nickname,
-                        'thumbnail': record.thumbnail
-                    })
-
-                return makeResponse(200, result)
-
-    except psycopg2.Error as e:
-        logger.error(f"get contacts error: {e}")
+        user_id = event['requestContext']['authorizer']['claims']['sub']
+        result = storage.list_contacts(user_id)
+        return makeResponse(200, result)
+    except Exception as e:
         return makeResponse(500, {'message': str(e)})
 
 
 def delete_contact(event, context):
+    storage = get_storage()
     try:
-        with closing(getConnection()) as connection:
-            with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
-                user_id = event['requestContext']['authorizer']['claims']['sub']
-                contact_id = event['pathParameters']['id']
-                schema = os.environ['PGSCHEMA']
+        user_id = event['requestContext']['authorizer']['claims']['sub']
+        contact_id = event['pathParameters']['id']
+        success = storage.remove_contact(user_id, contact_id)
 
-                cursor.execute(
-                    f'DELETE FROM "{schema}"."contact"'
-                    'WHERE "user_id"=%s AND "id"=%s AND '
-                    f'NOT EXISTS (SELECT 1 FROM "{schema}"."borrowing" WHERE'
-                    '"user_id"=%s AND "contact_id"=%s)',
-                    [user_id, contact_id, user_id, contact_id])
-                connection.commit()
-                if cursor.rowcount >= 1:
-                    return makeResponse(204)
-                else:
-                    return makeResponse(404)
-
-    except psycopg2.Error as e:
-        logger.error(f"delete contact error: {e}")
+        status = 204 if success is True else 404
+        return makeResponse(status)
+    except Exception as e:
         return makeResponse(500, {'message': str(e)})
---
+++ contacts/storage.py
@@ -0,0 +1,152 @@
+from abc import ABC, abstractmethod
+
+import psycopg2
+from contextlib import closing
+from psycopg2.extras import NamedTupleCursor
+import os
+import logging
+
+
+from contacts.utils import get_attribute
+
+logger = logging.getLogger()
+
+'''
+def getConnection():
+    hostname = os.environ['PGHOST']
+    port = os.environ['PGPORT']
+    database = os.environ['PGDATABASE']
+    user = os.environ['PGUSER']
+    password = os.environ['PGPASSWORD']
+
+    connection = psycopg2.connect(
+        host=f'{hostname}',
+        port=f'{port}',
+        database=f'{database}',
+        user=f'{user}',
+        password=f'{password}'
+    )
+
+    logger.debug(f'Connected successfully to database {database}')
+    return connection
+'''
+
+
+class Storage(ABC):
+
+    @abstractmethod
+    def remove_contact(self, user_id, contact_id):
+        pass
+
+    @abstractmethod
+    def list_contacts(self, user_id):
+        pass
+
+    @abstractmethod
+    def save_contact(self, user_id, id, contact):
+        pass
+
+    @abstractmethod
+    def modify_contact(self, user_id, contact):
+        pass
+
+
+class DatabaseStorage(Storage):
+    def __init__(self):
+        hostname = os.environ['PGHOST']
+        port = os.environ['PGPORT']
+        database = os.environ['PGDATABASE']
+        user = os.environ['PGUSER']
+        password = os.environ['PGPASSWORD']
+
+        self.connection = psycopg2.connect(
+            host=f'{hostname}',
+            port=f'{port}',
+            database=f'{database}',
+            user=f'{user}',
+            password=f'{password}'
+        )
+
+        logger.debug(f'Connected successfully to database {database}')
+
+    def remove_contact(self, user_id, contact_id):
+        try:
+            with closing(self.connection) as connection:
+                with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
+                    schema = os.environ['PGSCHEMA']
+
+                    cursor.execute(
+                        f'DELETE FROM "{schema}"."contact"'
+                        'WHERE "user_id"=%s AND "id"=%s AND '
+                        f'NOT EXISTS (SELECT 1 FROM "{schema}"."borrowing" WHERE'
+                        '"user_id"=%s AND "contact_id"=%s)',
+                        [user_id, contact_id, user_id, contact_id])
+                    connection.commit()
+                    return cursor.rowcount >= 1
+        except psycopg2.Error as e:
+            logger.error(f"delete contact error: {e}")
+            raise e
+
+    def list_contacts(self, user_id):
+        try:
+            with closing(self.connection) as connection:
+                with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
+
+                    schema = os.environ['PGSCHEMA']
+
+                    cursor.execute(
+                        f'SELECT "id", "nickname", "thumbnail" FROM "{schema}"."contact"'
+                        'WHERE "user_id"=%s ORDER BY "nickname"',
+                        [user_id])
+
+                    result = {
+                        'contacts': []
+                    }
+                    for record in cursor:
+                        result['contacts'].append({
+                            'id': record.id,
+                            'nickname': record.nickname,
+                            'thumbnail': record.thumbnail
+                        })
+
+                    return result
+        except psycopg2.Error as e:
+            logger.error(f"get contacts error: {e}")
+            raise e
+
+    def save_contact(self, user_id, id, contact):
+        try:
+            with closing(self.connection) as connection:
+                with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
+                    schema = os.environ['PGSCHEMA']
+                    nickname = get_attribute(contact, 'nickname', '')
+                    thumbnail = get_attribute(contact, 'thumbnail', None)
+                    cursor.execute(
+                        f'INSERT INTO "{schema}"."contact"(id, user_id, nickname, thumbnail) '
+                        "VALUES ( %s, %s, %s, %s); ",
+                        (id, user_id, nickname, thumbnail))
+                    connection.commit()
+        except psycopg2.Error as e:
+            logger.error(f"get contacts error: {e}")
+            raise e
+
+    def modify_contact(self, user_id, contact):
+        try:
+            with closing(self.connection) as connection:
+                with closing(connection.cursor(cursor_factory=NamedTupleCursor)) as cursor:
+                    schema = os.environ['PGSCHEMA']
+                    id = contact['id']
+                    nickname = get_attribute(contact, 'nickname', '')
+                    thumbnail = get_attribute(contact, 'thumbnail', None)
+                    cursor.execute(
+                        f'UPDATE "{schema}"."contact" '
+                        'SET "nickname"=%s, "thumbnail"=%s WHERE "id" = %s AND "user_id"=%s ;',
+                        (nickname, thumbnail, id, user_id))
+                    connection.commit()
+        except psycopg2.Error as e:
+            logger.error(f"get contacts error: {e}")
+            raise e
+
+
+def get_storage():
+    return DatabaseStorage()
